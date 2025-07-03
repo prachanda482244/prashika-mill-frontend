b320b715e18bc87f5d2923119ffcfe4eeab6c4ba
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2025-07-03 22:49:12 +0545

    enhance the loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,8 @@
-import React from "react";
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
-
 const Loader = () => {
   return (
-    <div className="flex items-center text-3xl min-h-screen justify-center min-w-full animate-spin">
-      <AiOutlineLoading3Quarters />
-    </div>
+      <div className="flex items-center justify-center h-64">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+          </div>
   );
 };
 
